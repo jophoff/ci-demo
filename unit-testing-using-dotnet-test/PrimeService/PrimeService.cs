67d978e0977b5b6603e8a6eea9fcb55492626ed9
Jacques Ophoff <j.ophoff@abertay.ac.uk>
2023-10-24 16:45:40 +0100

    Implemented method

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -6,7 +6,12 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
-            throw new NotImplementedException("Not implemented.");
+			if (candidate == 1)
+			{
+				return false;
+			}
+
+            throw new NotImplementedException("Not fully implemented.");
         }
     }
 }
